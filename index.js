3c9cb9e70d9967fd6aeef0ed40edeb66ac384cda
Hermes325 <georg3georg3georg@gmail.com>
2022-12-17 01:07:23 +0400

    Update

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "180px";
-}
+{/* <div class="flex_main">
+<div>
+    <div class="dvigaem Logo_media">
+        <h1>
+            Fly Boots
+        </h1>
+        <p class="jost400_2 margin_top10 more_media">Congue arcu aliquam amet sapien, sollicitudin egestas.
+            Sed volutpat tristique bibendum senectus consectetur sagittis, at.
+        </p>
+    </div>
+</div>
 
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+<div>
+    <div class="krugmain flex_center"><img src="./картинки/FBlogo.svg" alt="Logotip" class="Logotip"></div>
+
+</div>
+<div class="praviy_text_media">
+    <div class="text_top">
+        <p class="jost400_3" style="width: 26vw;">
+            Congue arcu aliquam amet sapien, sollicitudin egestas.
+            Sed volutpat tristique bibendum senectus consectetur sagittis, at.
+        </p>
+    </div>
+</div>
+<img src="./картинки/Subtract.png" alt="Шнурки" class="shnurki">
+<img src="./картинки/shnurki2.svg" alt="shnurki" class="shnurki2">
+
+<div class="more_media">
+    <div class="krug1"></div>
+    <div class="krug2"></div>
+    <div class="krug3"></div>
+    <div class="krug4"></div>
+</div>
+
+</div>
+ */}
