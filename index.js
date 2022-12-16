238b93c881dc3e71772cdb2362873209cea59bc4
Hermes325 <georg3georg3georg@gmail.com>
2022-12-17 03:30:36 +0400

    Update

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-{/* <div class="flex_main">
-<div>
-    <div class="dvigaem Logo_media">
-        <h1>
-            Fly Boots
-        </h1>
-        <p class="jost400_2 margin_top10 more_media">Congue arcu aliquam amet sapien, sollicitudin egestas.
-            Sed volutpat tristique bibendum senectus consectetur sagittis, at.
-        </p>
-    </div>
-</div>
 
-<div>
-    <div class="krugmain flex_center"><img src="./картинки/FBlogo.svg" alt="Logotip" class="Logotip"></div>
-
-</div>
-<div class="praviy_text_media">
-    <div class="text_top">
-        <p class="jost400_3" style="width: 26vw;">
-            Congue arcu aliquam amet sapien, sollicitudin egestas.
-            Sed volutpat tristique bibendum senectus consectetur sagittis, at.
-        </p>
-    </div>
-</div>
-<img src="./картинки/Subtract.png" alt="Шнурки" class="shnurki">
-<img src="./картинки/shnurki2.svg" alt="shnurki" class="shnurki2">
-
-<div class="more_media">
-    <div class="krug1"></div>
-    <div class="krug2"></div>
-    <div class="krug3"></div>
-    <div class="krug4"></div>
-</div>
-
-</div>
- */}
+/* Когда пользователь прокручивает вниз, скрыть навигационную панель. Когда пользователь прокручивает вверх, показать навигационную панель */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
